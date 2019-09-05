46926e4fa093a41b7f5d16b48fa244dc39386b3b
Muhammad Umar Farisi <pharese@gmail.com>
2019-09-06 00:01:48 +0700

    add codes

---
+++ GreetingApps/GreetingApps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GreetingApps
+//
+//  Created by muhammad.farisi on 05/09/19.
+//  Copyright © 2019 Pharese. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
